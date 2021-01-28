b558e4223b5b4c98de914046933b0b58f1f32f8d
Manuel Lora Román <manuelloraroman@gmail.com>
2021-01-28 20:08:43 +0100

    He añadido contenido nuevo

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9f0h)gozf$g%6igo8&767w1xro0adm+)msxe)!eic$!fhvynb8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
